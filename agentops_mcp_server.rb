eb374ccd6e15b6de81011a7ef9240433eb122f63
Rio Fujita <rio_github@rio.st>
2026-02-27 10:34:43 +0900

    add check for existing directory to zed-agentops-init

--- agentops_mcp_server.rb
+++ agentops_mcp_server.rb
@@ -3,8 +3,8 @@ class AgentopsMcpServer < Formula
 
   desc "AgentOps MCP Server"
   homepage "https://github.com/rioriost/homebrew-agentops_mcp_server/"
-  url "https://files.pythonhosted.org/packages/bd/ab/4783fd569f826186f312fe78ede9c17d775272ed425a5c0b667351120148/agentops_mcp_server-0.0.10.tar.gz"
-  sha256 "3ff7b40618c65c2c22fbccf5a35a1afca799635ddeac1d27a62a9efab01ad286"
+  url "https://files.pythonhosted.org/packages/0c/46/0183bc47dbd066606328aba358190bc753cdfaff1f3aac24653acaf956f5/agentops_mcp_server-0.0.11.tar.gz"
+  sha256 "ff030ef997f5a5d9c25d4a7bae4f85be4595de37d4d42788ab6f763ef7d143ec"
   license "MIT"
 
   depends_on "python@3.14"
