c8ab4bf60bbb26bc86f5bc50cbe01f02801e8430
Rio Fujita <rio_github@rio.st>
2026-03-04 12:40:01 +0900

    Remoced heredoc from zed-agentops-init

--- agentops_mcp_server.rb
+++ agentops_mcp_server.rb
@@ -3,8 +3,8 @@ class AgentopsMcpServer < Formula
 
   desc "AgentOps MCP Server"
   homepage "https://github.com/rioriost/homebrew-agentops_mcp_server/"
-  url "https://files.pythonhosted.org/packages/3e/a5/54dcad759816889544e1ee9de7ecf28274d65f9614c5d762a3c49bebbc06/agentops_mcp_server-0.1.5.tar.gz"
-  sha256 "2137da08a8eea16034f2d7c5cfbdf047e0adc59bd2993ee506ac137c09bfaef9"
+  url "https://files.pythonhosted.org/packages/09/57/cc549df12aa85ee043ffeb13526f86067633551c57ca72294ed85b87319c/agentops_mcp_server-0.1.6.tar.gz"
+  sha256 "8359b92f3ea5e1b54d54c65bda7569178b9515e0741431dbca41c8e0ebd565ce"
   license "MIT"
 
   depends_on "python@3.14"
