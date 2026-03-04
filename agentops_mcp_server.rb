0242537dcbae95fb702e2f32bba5f9785865d197
Rio Fujita <rio_github@rio.st>
2026-03-04 22:10:09 +0900

    sync zed .rules template with GPT-5.3-Codex EXECUTE rule

--- agentops_mcp_server.rb
+++ agentops_mcp_server.rb
@@ -3,8 +3,8 @@ class AgentopsMcpServer < Formula
 
   desc "AgentOps MCP Server"
   homepage "https://github.com/rioriost/homebrew-agentops_mcp_server/"
-  url "https://files.pythonhosted.org/packages/09/57/cc549df12aa85ee043ffeb13526f86067633551c57ca72294ed85b87319c/agentops_mcp_server-0.1.6.tar.gz"
-  sha256 "8359b92f3ea5e1b54d54c65bda7569178b9515e0741431dbca41c8e0ebd565ce"
+  url "https://files.pythonhosted.org/packages/09/b5/3e9908fc7fad150491ecbb42fd70bcba1162a59735c81783ac9f5f4fb077/agentops_mcp_server-0.1.7.tar.gz"
+  sha256 "c2c644084a6481a18990f7dc277ce0ddf36b530dfa6ee273a091885fc1ecde49"
   license "MIT"
 
   depends_on "python@3.14"
