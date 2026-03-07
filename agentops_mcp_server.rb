70d4dec0b8f175f34772587023e314157df3fdeb
Rio Fujita <rio_github@rio.st>
2026-03-07 16:34:32 +0900

    0.4.1

--- agentops_mcp_server.rb
+++ agentops_mcp_server.rb
@@ -3,8 +3,8 @@ class AgentopsMcpServer < Formula
 
   desc "AgentOps MCP Server"
   homepage "https://github.com/rioriost/homebrew-agentops_mcp_server/"
-  url "https://files.pythonhosted.org/packages/b2/98/8d961cebb03d9e587e36154f678e86b068a9067d49a2db40990c93269c7e/agentops_mcp_server-0.3.1.tar.gz"
-  sha256 "e946e93bb18563eaf248c3be8d9e1c124f5c9c83c724b36bd4dc422f8a93164a"
+  url "https://files.pythonhosted.org/packages/82/fe/a46e63d989f1da402ef6390346adcd56788f13d715d22dd0e2035b5f8ad1/agentops_mcp_server-0.4.1.tar.gz"
+  sha256 "1309840082d64b51e611551f5db3c13c9507fa464bb830ae4804c710c3845e7c"
   license "MIT"
 
   depends_on "python@3.14"
