587afe1a89330709b10fe608582cc1ee4cc5c6b7
Rio Fujita <rio_github@rio.st>
2026-03-08 16:20:03 +0900

    no longer to create journal.jsonl

--- agentops_mcp_server.rb
+++ agentops_mcp_server.rb
@@ -3,8 +3,8 @@ class AgentopsMcpServer < Formula
 
   desc "AgentOps MCP Server"
   homepage "https://github.com/rioriost/homebrew-agentops_mcp_server/"
-  url "https://files.pythonhosted.org/packages/a9/63/20d5c922f3467f73ba1205c27bc7fa91bfe7f4067b59cf6ec1732dd0f459/agentops_mcp_server-0.4.9.tar.gz"
-  sha256 "3e8e8cd90c9f57b9f1a2c00e3f20410defec850cc49d6f152f8a756a5e38e835"
+  url "https://files.pythonhosted.org/packages/06/22/bd2138f0aaac4cf1b233ad2ff39a4ae6b385b83e12b614214f6dc27ac135/agentops_mcp_server-0.4.10.tar.gz"
+  sha256 "241d3191ffe77c3304979a0b410d001333703f300082614c2db5767ea237c979"
   license "MIT"
 
   depends_on "python@3.14"
